1784bc8907dcc111bde404cbf316fd339d485873
Ralph Roberts <ralph.roberts5@gmail.com>
2018-01-21 22:38:02 -0800

    added jquery code to modify strike through and delete to do items, also added input function to add new items

---
+++ checklist/checklistjs.js
@@ -0,0 +1,17 @@
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(1000, function(){
+		$(this).remove();
+	})
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(enter){
+	if(enter.which === 13){
+		var newtext = $(this).val();
+		$("ul").append("<li><span>X </span>" + newtext + "</li>")
+	}
+})
